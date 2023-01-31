a541dfda8eaf98a3ed934488d1aceeea6893df15
Maciej Faron <58699402+MaciejFaron@users.noreply.github.com>
2023-01-31 22:29:17 +0100

    Add collision detection with obstacle

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    double nextPrint = 0;
+    private double nextPrint = 0;
+    //private List
+    public Spawner obstacleSpawner;
+    public PlayerSpawner playerSpawner;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        //obstacleSpawner.subscribeToSpawnAction(this);
+        playerSpawner.subscribeToSpawnAction(this);
+    }
+
+    // called on every obstacle GameObject spawn
+    void OnObstacleSpawnAction() { 
+    }
+
+    //called when new player character is spawned
+    void OnPlayerSpawnAction() {
+        //called when new player character is spawned
+        //allow 
+
+    }
+    void OnPlayerDeathAction() {
+        //turn off player visibility
+        //turn off collision
+        //resset position
     }
 
     // Update is called once per frame
@@ -17,7 +37,7 @@ public class GameManager : MonoBehaviour
     {
         double timeAsDouble = Time.timeAsDouble;
         if (timeAsDouble > nextPrint) {
-            Debug.Log("Time.timeAsDouble: " + timeAsDouble);
+            //Debug.Log("Time.timeAsDouble: " + timeAsDouble);
             nextPrint++;
         }
     }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,19 +6,41 @@ public class Player : MonoBehaviour
 {
     public float velocity = 2.4f;
     private Rigidbody2D rigidbody;
+
+    // Awake is called when the script instance is being loaded.
+    void Awake() { 
+        
+    }
     
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         if (Input.GetKeyDown("space")) {
             Vector2 _velocity = Vector2.up * velocity;
             rigidbody.velocity = _velocity;
         }
     }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        string collidedWithName = collision.collider.name;
+        string collidedOtherName = collision.otherCollider.name;
+        if (collision.gameObject.CompareTag("Obstacle")) {
+            print($"Collision {collidedWithName}, {collidedOtherName}");
+            this.PlayersDeath();
+        }
+        //if so player death
+    }
+
+    void PlayersDeath() {
+        print($"Player died due collision.");
+        //turn off collision
+        //hide player
+        //reset position
+        //wait for GameManager to restart the game
+    }
+
 }
